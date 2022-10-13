cc9a521f5f97fa1eb354a82a0b73c2f95fb06ca4
stagnant-sys <paysac.thomas@gmail.com>
2022-10-13 15:13:05 +0200

    Working playround

---
+++ script.js
@@ -0,0 +1,96 @@
+let computerRdn
+let computerSelection
+let playerChoice
+let playerSelection
+let playerScore = 0
+let computerScore = 0
+let roundNumber
+
+function playRound() { // Complete round, using functions defined later
+getComputerChoice()
+getPlayerChoice()
+battle()
+}
+
+// OK! randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. Store result in computerSelection
+function getComputerChoice() { 
+    let computerRdn = Math.floor(Math.random() * (3 - 1 + 1) ) + 1;
+    switch(computerRdn){
+        case 1 :
+            computerSelection = 'rock';
+            break;
+        case 2 :
+            computerSelection = 'paper';
+            break;
+        case 3 :
+            computerSelection = 'scissors';
+            break;
+        default : console.log('There was an error.');
+    }
+}
+
+
+// OK ! prompt the user to return either 'Rock', 'Paper', 'Scissors'. Store in playerSelection. !! Make it case insensitive !!
+function getPlayerChoice() {    
+    playerChoice = prompt('Choose your weapon : Rock, Paper or Scissors?', ''); // player's choice
+    playerSelection = playerChoice.toLowerCase(); // convert the answer to lowercase
+    
+    if (playerSelection === 'rock' || playerSelection === 'paper' || playerSelection === 'scissors') { // check if answer is valid
+    console.log('Get ready to fight !');
+    } else {
+        alert('Don\'t be a coward ! Choose either Rock, Paper or Scissors.')
+        getPlayerChoice();
+}
+}
+
+// compare computerSelection and playerSelection
+function battle(){
+    if (computerSelection == 'rock'){ // computer chose rock
+    switch(playerSelection){
+        case 'rock':
+            console.log('Incredible ! It\'s a draw!');
+            break;
+        case 'paper':
+            console.log('You win this one! Nice job.')
+            playerScore += 1;
+            break;
+        case 'scissors':
+            console.log('Ouch, that must have hurt... You lost this one.')
+            computerScore += 1;
+            break;
+    }
+
+} else if (computerSelection == 'paper'){ // computer chose paper
+    switch(playerSelection){
+    case 'rock':
+        console.log('Ouch, that must have hurt... You lost this one.');
+        computerScore += 1
+        break;
+    case 'paper':
+        console.log('Incredible ! It\'s a draw!')
+        break;
+    case 'scissors':
+        console.log('You win this one! Nice job.')
+        playerScore += 1;
+        break;
+}
+} else if (computerSelection == 'scissors') { // computer chose scissors
+    switch(playerSelection){
+    case 'rock':
+        console.log('You win this one! Nice job.');
+        playerScore += 1;
+        break;
+    case 'paper':
+        console.log('Ouch, that must have hurt... You lost this one.')
+        computerScore += 1
+        break;
+    case 'scissors':
+        console.log('Incredible ! It\'s a draw!')
+        break;
+    }
+}
+}
+
+
+
+// count rounds to a maximum of 5: function game(playRound()), loop for 5 games, returns final score at the end
